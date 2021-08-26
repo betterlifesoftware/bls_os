a2c1339cfbcf7b95be0955a78e0404e92dd6fc11
gabbo2001 <39064880+gabbo2001@users.noreply.github.com>
2021-08-26 20:46:02 +0200

    Update wait_printing.sh

--- wait_printing.sh
+++ wait_printing.sh
@@ -9,7 +9,7 @@
 # Description:       Something else
 ### END INIT INFO
 
-inotifywait -m /printing |
+inotifywait -m /printing -e create -e moved_to |
     while read dir action file; do
         echo "The file '$file' appeared in directory '$dir' via '$action'"
         echo $file
